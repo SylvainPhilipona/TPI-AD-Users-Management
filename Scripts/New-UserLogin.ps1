4e6b8a67527c78709532f650b1b69c145aa46bac
SylvainPhilipona <sylvain.philipona.sp@gmail.com>
2023-05-10 14:05:29 +0200

    Dev: login generation + tests
    User login generation and units testing

---
+++ Scripts/New-UserLogin.ps1
@@ -0,0 +1,78 @@
+﻿<#
+.NOTES
+    *****************************************************************************
+    ETML
+    Name:	New-UserLogin.ps1
+    Author:	Sylvain Philipona
+    Date:	10.05.2023
+ 	*****************************************************************************
+    Modifications
+ 	Date  : 
+ 	Author: 
+ 	Reason: 
+ 	*****************************************************************************
+.SYNOPSIS
+    Generate a login name based on a firstname and lastname
+ 	
+.DESCRIPTION
+    The login name is generated from the first 3 characters of the first name and the first 8 characters (maximum) of the last name
+  	
+.PARAMETER FirstName
+    The firstname of the user
+
+.PARAMETER LastName
+    The lastname of the user
+
+.OUTPUTS
+	- The generated login from the firstname and lastname
+
+.EXAMPLE
+    .\New-UserLogin.ps1 -FirstName "John" -LastName "Doe"
+    johdoe
+ 	
+.EXAMPLE
+    .\New-UserLogin.ps1 -FirstName "Sylvain" -LastName "Philipona"
+    sylphilipon
+#>
+
+##### Script parameters #####
+
+param(
+    [ValidateNotNullOrEmpty()]
+    [string]$FirstName,
+
+    [ValidateNotNullOrEmpty()]
+    [string]$LastName
+)
+
+##### Variables #####
+
+$login = ""
+
+##### Script logic #####
+
+# Get the first 3 characters of the firstname
+# If the firstname do not contains at least 3 characters, all the firstname is taken
+if ($FirstName.Length -gt 3) {
+    # Add to the login string the 3 first chars
+    $login += $FirstName.substring(0,3)
+} else {
+    # Add to the login string the entire firstname
+    $login += $FirstName
+}
+
+# Get the 8 first chars of the lastname
+# If the lastname do not contains at least 8 chars, all the lastname is taken
+if ($LastName.Length -gt 8) {
+    # Add to the login string the 8 first chars
+    $login += $LastName.substring(0,8)
+} else {
+    # Add to the login string the entire lastName
+    $login += $LastName
+}
+
+# Put the login in lowercase
+$login = $login.ToLower()
+
+# Return the formatted login
+return $login
---
+++ Scripts/New-UserLogin.test.ps1
@@ -0,0 +1,75 @@
+﻿<#
+.NOTES
+    *****************************************************************************
+    Name:	New-UserLogin.test.ps1
+    Author:	Sylvain Philipona
+    Date:	10.05.2023
+ 	*****************************************************************************
+    Modifications
+ 	Date  : 
+ 	Author: 
+ 	Reason: 
+ 	*****************************************************************************
+.SYNOPSIS
+    Unit tests for the New-UserLogin.ps1 script
+ 	
+.DESCRIPTION
+    A list of unit tests for the New-UserLogin.ps1 script
+    A big range of inputs possibilities are tested
+ 	
+.OUTPUTS
+	The result of units tests
+
+.EXAMPLE
+    .\New-UserLogin.test.ps1
+
+    Starting discovery in 1 files.
+    Discovery found 7 tests in 12ms.
+    Running tests.
+    [+] C:\Users\sylphilipona\GitHub\TPI-AD-Users-Management\Scripts\New-UserLogin.test.ps1 70ms (25ms|35ms)
+    Tests completed in 73ms
+    Tests Passed: 7, Failed: 0, Skipped: 0 NotRun: 0
+
+.LINK
+    https://pester.dev/docs/quick-start
+    https://pester.dev/docs/v4/usage/assertions
+#>
+
+
+# Test multiple differents cases
+Describe "Tests"{
+    # Sylvain Philipona => sylphilipon
+    It "Sylvain Philipona => sylphilipon"{
+        (.\New-UserLogin.ps1 -FirstName "Sylvain" -LastName "Philipona") | Should -Be "sylphilipon"
+    }
+
+    # John Doe => johdoe
+    It "John Doe => johdoe"{
+        (.\New-UserLogin.ps1 -FirstName "John" -LastName "Doe") | Should -Be "johdoe"
+    }
+
+    # JOHN DOE => johdoe
+    It "JOHN DOE => johdoe"{
+        (.\New-UserLogin.ps1 -LastName "DOE"  -FirstName "JOHN") | Should -Be "johdoe"
+    }
+
+    # Alessandro D'angélo => aled'angélo
+    It "Alessandro D'angélo => aled'angélo"{
+        (.\New-UserLogin.ps1 -FirstName "Alessandro" -LastName "D'angélo") | Should -Be "aled'angélo"
+    }
+
+    # Jo Chen => jochen
+    It "Jo Chen => jochen"{
+        (.\New-UserLogin.ps1 -FirstName "Jo" -LastName "Chen") | Should -Be "jochen"
+    }
+
+    # Fernandez Charpentier => fercharpent
+    It "Fernandez Charpentier => fercharpent"{
+        (.\New-UserLogin.ps1 -FirstName "Fernandez" -LastName "Charpentier") | Should -Be "fercharpent"
+    }
+
+    # No arguments => empty string
+    It "No arguments => empty string"{
+        (.\New-UserLogin.ps1) | Should -Be ""
+    }
+}
