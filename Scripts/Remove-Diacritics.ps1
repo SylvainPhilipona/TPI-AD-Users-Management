1f296b48cf684c6feb0d58e711cf8f60e26cabfc
SylvainPhilipona <sylvain.philipona.sp@gmail.com>
2023-05-11 15:41:39 +0200

    Dev: Remove string diacritics + UTF-8 With BOM encoding

---
+++ Scripts/Remove-Diacritics.ps1
@@ -0,0 +1,76 @@
+﻿<#
+.NOTES
+    *****************************************************************************
+    ETML
+    Name:	Remove-Diacritics.ps1
+    Author:	Sylvain Philipona
+    Date:	11.05.2023
+ 	*****************************************************************************
+    Modifications
+ 	Date  : 
+ 	Author: 
+ 	Reason: 
+ 	*****************************************************************************
+.SYNOPSIS
+    Remove the diacritics in a string
+ 	
+.DESCRIPTION
+    Remove all the diacritics, spaces and punctuation marks in a string
+    This use the Unicode normalization
+
+.PARAMETER Text
+    The input string that will have diacritics removed
+
+.OUTPUTS
+	- The string with diacritics removed
+
+.EXAMPLE
+    .\Remove-Diacritics.ps1 -Text "L'été de Raphaël"
+    
+    LetedeRaphael
+
+.LINK
+    https://lazywinadmin.com/2015/05/powershell-remove-diacritics-accents.html
+    https://learn.microsoft.com/fr-fr/dotnet/api/system.string.normalize?view=net-8.0
+    https://unicode.org/reports/tr15/
+    https://learn.microsoft.com/en-us/dotnet/api/system.globalization.unicodecategory?view=net-8.0
+#>
+
+##### Script parameters #####
+
+param(
+    [ValidateNotNullOrEmpty()]
+    [string]$Text
+)
+
+##### Variables #####
+
+$normalizedText = ""
+
+##### Script logic #####
+
+# Remove the spaces and punctuation marks
+$Text = $Text.Replace("'", "")
+$Text = $Text.Replace(" ", "")
+
+# Split characters composed of several characters (é => e ´)
+# Example : L'été de Raphaël => L ' e ́ t e ́   d e   R a p h a e ̈ l
+[System.Text.NormalizationForm]$NormalizationForm = "FormD"
+
+# Normalize the text
+$Normalized = $Text.Normalize($NormalizationForm)
+
+# Iterates through all text in the format of a character array
+foreach($char in $normalized.ToCharArray()){
+
+    # Nonspacing character that indicates modifications of a base character
+    # Check if the character isn't nonspacing
+    if ([Globalization.CharUnicodeInfo]::GetUnicodeCategory($char) -ne [Globalization.UnicodeCategory]::NonSpacingMark)
+    {
+        # Add the nonspacing character to the result string
+        $normalizedText += $char
+    }
+}
+
+# Return the string without diacritics, spaces and punctuation marks
+return $normalizedText
---
+++ Scripts/Remove-Diacritics.test.ps1
@@ -0,0 +1,55 @@
+﻿<#
+.NOTES
+    *****************************************************************************
+    Name:	Remove-Diacritics.test.ps1
+    Author:	Sylvain Philipona
+    Date:	11.05.2023
+ 	*****************************************************************************
+    Modifications
+ 	Date  : 
+ 	Author: 
+ 	Reason: 
+ 	*****************************************************************************
+.SYNOPSIS
+    Unit tests for the Remove-Diacritics.test.ps1 script
+ 	
+.DESCRIPTION
+    A list of unit tests for the Remove-Diacritics.test.ps1 script
+    A big range of inputs possibilities are tested
+ 	
+.OUTPUTS
+	The result of units tests
+
+.EXAMPLE
+    .\Remove-Diacritics.test.ps1
+
+    Starting discovery in 1 files.
+    Discovery found 3 tests in 17ms.
+    Running tests.
+    [+] C:\Users\sylphilipona\GitHub\TPI-AD-Users-Management\Scripts\Remove-Diacritics.test.ps1 62ms (15ms|32ms)
+    Tests completed in 65ms
+    Tests Passed: 3, Failed: 0, Skipped: 0 NotRun: 0
+
+.LINK
+    https://pester.dev/docs/quick-start
+    https://pester.dev/docs/v4/usage/assertions
+#>
+
+# Test multiple differents cases
+Describe "Tests"{
+
+    # L'été de Raphaël => LetedeRaphael
+    It "L'été de Raphaël => LetedeRaphael"{
+        (.\Remove-Diacritics.ps1 -Text "L'été de Raphaël") | Should -Be "LetedeRaphael"
+    }
+
+    # ÚéõãÎÇĠ => UeoaICG
+    It "ÚéõãÎÇĠ => UeoaICG"{
+        (.\Remove-Diacritics.ps1 -Text "ÚéõãÎÇĠ") | Should -Be "UeoaICG"
+    }
+
+    # Alessandro D'angélo => AlessandroDangelo
+    It "Alessandro D'angélo => AlessandroDangelo"{
+        (.\Remove-Diacritics.ps1 -Text "Alessandro D'angélo") | Should -Be "AlessandroDangelo"
+    }
+}
